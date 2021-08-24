187f628847483973c34078dbc073d19096a462b1
agenis <artyomgenis@gmail.com>
2021-08-24 17:49:31 +0300

    collection create/edit fix 1

--- src/pages/EditCollection.js
+++ src/pages/EditCollection.js
@@ -62,7 +62,7 @@ function EditCollection() {
     const onSubmit = (data) => {
         data.description = freeText;
         axios.post(`https://itransition-project-genis.herokuapp.com/collections/${id}/editCollection`, data).then(() => {
-            history.push(`/user/${authState.id}`);
+            history.push(`/user/${id}`);
         });
     };
 
