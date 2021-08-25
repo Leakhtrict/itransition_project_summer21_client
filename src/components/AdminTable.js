9a047a271632602850ad29bbdd608c405b7743a8
agenis <artyomgenis@gmail.com>
2021-08-25 15:23:28 +0300

    admin table fix 3.9

--- src/components/AdminTable.js
+++ src/components/AdminTable.js
@@ -8,23 +8,6 @@ import SecurityIcon from '@material-ui/icons/Security';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import { DataGrid } from "@material-ui/data-grid";
 
-/*const SelectCheckbox = forwardRef(
-    ({ indeterminate, ...rest }, ref) => {
-      const defaultRef = useRef()
-      const resolvedRef = ref || defaultRef
-  
-      useEffect(() => {
-        resolvedRef.current.indeterminate = indeterminate
-      }, [resolvedRef, indeterminate])
-  
-      return (
-        <>
-          <input type="checkbox" ref={resolvedRef} {...rest} />
-        </>
-      )
-    }
-  );*/
-
 function AdminTable() {
     let history = useHistory();
     const [listOfUsers, setListOfUsers] = useState([]);
@@ -36,132 +19,123 @@ function AdminTable() {
         });
     }, []);
 
-    /*const tableInstance = useTable({
-        columns: COLUMNS,
-        data: listOfUsers
-      },
-        useRowSelect,
-        (hooks) => {
-          hooks.visibleColumns.push((columns) => {
-            return [
-              {
-                id: "selection",
-                Header: ({ getToggleAllRowsSelectedProps }) => (
-                  <SelectCheckbox {...getToggleAllRowsSelectedProps()} />
-                ),
-                Cell: ({ row }) => (
-                  <SelectCheckbox {...row.getToggleRowSelectedProps()} />
-                )
-              },
-              ...columns,
-            ];
+    const deleteData = () => {
+      axios.get("https://itransition-project-genis.herokuapp.com/users/auth",
+      { headers: { accessToken: localStorage.getItem("accessToken") } })
+      .then((response) => {
+        if (!response.data.error) {
+          const deleteIds = listOfIds;
+          axios.put("https://itransition-project-genis.herokuapp.com/users/deleteUsers", deleteIds)
+          .then(() => {
+            deleteIds.map((value) => {
+              setListOfUsers(listOfUsers.filter((user) => {
+                return user.id != value;
+              }))
+            });
           });
         }
-      );
-  
-      const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,
-        selectedFlatRows,
-      } = tableInstance;*/
+        else{
+          history.push("/");
+        }
+      });
+    };
 
-      const deleteData = () => {
-        axios.get("https://itransition-project-genis.herokuapp.com/users/auth",
-        { headers: { accessToken: localStorage.getItem("accessToken") } })
-        .then((response) => {
-          if (!response.data.error) {
-            const deleteIds = listOfIds;
-            axios.put("https://itransition-project-genis.herokuapp.com/users/deleteUsers", deleteIds)
-            /*.then(() => {
-              history.go(0);
-            });*/
-          }
-          else{
-            history.push("/");
-          }
-        });
-      };
-  
-      const blockUser = () => {
-        axios.get("https://itransition-project-genis.herokuapp.com/users/auth",
-        { headers: { accessToken: localStorage.getItem("accessToken") } })
-        .then((response) => {
-          if (!response.data.error) {
-            const blockIds = listOfIds;
-            axios.put("https://itransition-project-genis.herokuapp.com/users/blockUsers", blockIds)
+    const blockUser = () => {
+      axios.get("https://itransition-project-genis.herokuapp.com/users/auth",
+      { headers: { accessToken: localStorage.getItem("accessToken") } })
+      .then((response) => {
+        if (!response.data.error) {
+          const blockIds = listOfIds;
+          axios.put("https://itransition-project-genis.herokuapp.com/users/blockUsers", blockIds)
+          .then(() => {
             blockIds.map((value) => {
               setListOfUsers(listOfUsers.map((user) => {
                 if(value == user.id){
-                  return [...user, { isBlocked: true }]
+                  return {...user, isBlocked: true };
                 } else {
                   return user;
                 }
               }))
             });
-          }
-          else{
-            history.push("/");
-          }
-        });
-      };
-  
-      const unblockUser = () => {
-        axios.get("https://itransition-project-genis.herokuapp.com/users/auth",
-        { headers: { accessToken: localStorage.getItem("accessToken") } })
-        .then((response) => {
-          if (!response.data.error) {
-            const unblockIds = listOfIds;
-            axios.put("https://itransition-project-genis.herokuapp.com/users/unblockUsers", unblockIds)
-            /*.then(() => {
-              history.go(0);
-            });*/
-          }
-          else{
-            history.push("/");
-          }
-        });
-      };
+          });
+        }
+        else{
+          history.push("/");
+        }
+      });
+    };
 
-      const adminUser = () => {
-        axios.get("https://itransition-project-genis.herokuapp.com/users/auth",
-        { headers: { accessToken: localStorage.getItem("accessToken") } })
-        .then((response) => {
-          if (!response.data.error) {
-            const adminIds = listOfIds;
-            axios.put("https://itransition-project-genis.herokuapp.com/users/adminUsers", adminIds)
-            /*.then(() => {
-              history.go(0);
-            });*/
-          }
-          else{
-            history.push("/");
-          }
-        });
-      };
+    const unblockUser = () => {
+      axios.get("https://itransition-project-genis.herokuapp.com/users/auth",
+      { headers: { accessToken: localStorage.getItem("accessToken") } })
+      .then((response) => {
+        if (!response.data.error) {
+          const unblockIds = listOfIds;
+          axios.put("https://itransition-project-genis.herokuapp.com/users/unblockUsers", unblockIds)
+          .then(() => {
+            unblockIds.map((value) => {
+              setListOfUsers(listOfUsers.map((user) => {
+                if(value == user.id){
+                  return {...user, isBlocked: false };
+                } else {
+                  return user;
+                }
+              }))
+            });
+          });
+        }
+        else{
+          history.push("/");
+        }
+      });
+    };
 
-      return(
-        <>
-          <div className="buttonBar">
-            <LockIcon className="buttons" onClick={blockUser} title="Block" />
-            <LockOpenIcon className="buttons" onClick={unblockUser} title="Unblock" />
-            <SecurityIcon className="buttons" onClick={adminUser} title="Set admin" />
-            <DeleteForeverIcon className="buttons" onClick={deleteData} title="Delete" />
-          </div>
-          <div style={{ height: 500, width: 872 }} >
-            <DataGrid
-              rows={listOfUsers}
-              columns={COLUMNS}
-              checkboxSelection
-              disableSelectionOnClick
-              disableColumnMenu
-              onSelectionModelChange={itm => setListOfIds(itm)}
-            />
-          </div>
-        </>
-    );
+    const adminUser = () => {
+      axios.get("https://itransition-project-genis.herokuapp.com/users/auth",
+      { headers: { accessToken: localStorage.getItem("accessToken") } })
+      .then((response) => {
+        if (!response.data.error) {
+          const adminIds = listOfIds;
+          axios.put("https://itransition-project-genis.herokuapp.com/users/adminUsers", adminIds)
+          .then(() => {
+            adminIds.map((value) => {
+              setListOfUsers(listOfUsers.map((user) => {
+                if(value == user.id){
+                  return {...user, isAdmin: true };
+                } else {
+                  return user;
+                }
+              }))
+            });
+          });
+        }
+        else{
+          history.push("/");
+        }
+      });
+    };
+
+    return(
+      <>
+        <div className="buttonBar">
+          <LockIcon className="buttons" onClick={blockUser} title="Block" />
+          <LockOpenIcon className="buttons" onClick={unblockUser} title="Unblock" />
+          <SecurityIcon className="buttons" onClick={adminUser} title="Set admin" />
+          <DeleteForeverIcon className="buttons" onClick={deleteData} title="Delete" />
+        </div>
+        <div style={{ height: 630, width: 872 }} >
+          <DataGrid
+            rows={listOfUsers}
+            columns={COLUMNS}
+            pageSize={10}
+            checkboxSelection
+            disableSelectionOnClick
+            disableColumnMenu
+            onSelectionModelChange={itm => setListOfIds(itm)}
+          />
+        </div>
+      </>
+  );
 };
 
 export default AdminTable;
