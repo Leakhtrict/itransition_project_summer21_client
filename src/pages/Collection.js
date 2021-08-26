17725eb236966caaf95262c583c3764558771e9a
agenis <artyomgenis@gmail.com>
2021-08-27 00:04:37 +0300

    item visual fix 1.0.1

--- src/pages/Collection.js
+++ src/pages/Collection.js
@@ -82,7 +82,7 @@ function Collection() {
 
     return (
         <Container maxWidth="xs">
-            <Grid container direction="column" justifyContent="center" spacing={1}>
+            <Grid container direction="column" justifyContent="center" spacing={1} style={{ marginLeft: "16px" }}>
                 <Grid item>
                     {(authState.id === collectionObj.UserId || authState.isAdmin) && 
                         <Button onClick={() => {history.push(`/collection/${id}/createitem`)}} className={classes.buttonBar}>
@@ -94,43 +94,45 @@ function Collection() {
                     <SortSelect thisItems={thisItems} setThisItems={setThisItems} />
                 </Grid>
             </Grid>
-            <Grid container direction="column" justifyContent="center" spacing={1}>
-                {thisItems.map((value, key) => {
-                    const thisTags = value.tags.split(" ").slice(0, -1);
-                    return (
-                        <div key={key}>
-                            {(authState.id === collectionObj.UserId || authState.isAdmin) && 
-                                <>
-                                    <IconButton onClick={() => {history.push(`/collection/${id}/item/${value.id}/edit`)}} className={classes.itemButtons}>
-                                        <EditIcon />
-                                    </IconButton>
-                                    <IconButton onClick={() => deleteItem(value.id)} className={classes.itemButtons}>
-                                        <DeleteIcon />
-                                    </IconButton>
-                                </>
-                            }
-                            <Grid item container>
-                                <Box className="item">
-                                    <header onClick={() => {history.push(`/item/${value.id}`)}}>{value.name}</header>
-                                    <Grid item container style={{ margin: "10px" }} justifyContent="flex-start" alignItems="flex-start">
-                                        {thisTags.map((value, key) => {
-                                            return(
-                                                <Grid item key={key} onClick={() => {history.push(`/byTag/${value}`)}} className="itemTag" style={{ margin: "6px" }}>
-                                                    {"#" + value}
-                                                </Grid>
-                                            )
-                                        })}
-                                    </Grid>
-                                    <footer>
-                                        <FavoriteBorderIcon style={{ color: "red" }} />
-                                        {value.Likes.length}
-                                    </footer>
-                                </Box>
-                            </Grid>
-                        </div>
-                    );
-                })}
-            </Grid>
+            <Container maxWidth="xs" style={{ marginTop: "8px" }}>
+                <Grid container direction="column" justifyContent="center" spacing={1}>
+                    {thisItems.map((value, key) => {
+                        const thisTags = value.tags.split(" ").slice(0, -1);
+                        return (
+                            <div key={key}>
+                                {(authState.id === collectionObj.UserId || authState.isAdmin) && 
+                                    <>
+                                        <IconButton onClick={() => {history.push(`/collection/${id}/item/${value.id}/edit`)}} className={classes.itemButtons}>
+                                            <EditIcon />
+                                        </IconButton>
+                                        <IconButton onClick={() => deleteItem(value.id)} className={classes.itemButtons}>
+                                            <DeleteIcon />
+                                        </IconButton>
+                                    </>
+                                }
+                                <Grid item container style={{ marginBottom: "8px" }}>
+                                    <Box className="item">
+                                        <header onClick={() => {history.push(`/item/${value.id}`)}}>{value.name}</header>
+                                        <Grid item container style={{ margin: "10px" }} justifyContent="flex-start" alignItems="flex-start">
+                                            {thisTags.map((value, key) => {
+                                                return(
+                                                    <Grid item key={key} onClick={() => {history.push(`/byTag/${value}`)}} className="itemTag" style={{ margin: "6px" }}>
+                                                        {"#" + value}
+                                                    </Grid>
+                                                )
+                                            })}
+                                        </Grid>
+                                        <footer>
+                                            <FavoriteBorderIcon style={{ color: "red" }} />
+                                            {value.Likes.length}
+                                        </footer>
+                                    </Box>
+                                </Grid>
+                            </div>
+                        );
+                    })}
+                </Grid>
+            </Container>
         </Container>
     );
 }
